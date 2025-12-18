d74c0fcad99acb2f0b1d8f901ff16c02681b8490
M4THIA5 <mathias.collas@gmail.com>
2025-12-18 10:47:32 +0100

    first commit

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import express from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const app = async () => {
+  const app = express()
+  const port = process.env.PORT || 3000
+
+  app.use(cors())
+  app.use(express.json())
+
+  app.get("/", (req, res) => {
+    res.send("Hello, World!")
+  })
+
+  app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`)
+  })
+}
+
+app()
